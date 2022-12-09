65d21c2ae89d4a03f0a698f058863bec15a0a93a
Konrad <konswic@gmail.com>
2022-12-08 16:34:44 -0800

    Done

---
+++ fake_mapping.py
@@ -0,0 +1,75 @@
+mapping = {
+    'Bageldom1324 Z1': {
+        'devices': ['E6:9F:F3:C4:65:C8', '5F:EA:6A:48:75:E0', 'E1:5B:48:7A:7C:4C', 'FF:51:56:99:B4:C1'],
+        'table': 'Bageldom',
+        'site': 'Floor 1',
+        'site_id': 'Bageldom_L1_S1',
+        'zone': 'Salmon room',
+        'zone_id': 'Bageldomd_L1_S1_Z1',
+        'mac': 'DE:38:3C:CA:68:3F'
+    },
+    'Bageldom1324 Z2': {
+        'devices': ['6C:7F:5F:E1:9F:D1', '96:2A:AD:B8:40:C0', 'E0:56:6D:28:90:00', '60:0C:D2:23:10:18'],
+        'table': 'Bageldom',
+        'site': 'Floor 2',
+        'site_id': 'Bageldom_L1_S2',
+        'zone': 'Creamery',
+        'zone_id': 'Bageldomd_L1_S2_Z1',
+        'mac': 'FF:CF:EE:7B:BD:87'
+    },
+    'Bageldom1324 Z3': {
+        'devices': ['46:B0:8B:48:DF:A6', 'E1:F1:B9:A8:74:62', '8B:73:DF:7A:90:54', '71:BB:BF:B4:02:05'],
+        'table': 'Bageldom',
+        'site': 'Floor 2',
+        'site_id': 'Bageldom_L1_S2',
+        'zone': 'Capers Room',
+        'zone_id': 'Bageldomd_L1_S2_Z2',
+        'mac': '0E:BA:3A:B6:0E:AD'
+    },
+    'Bageldom5634 Z1': {
+        'devices': ['49:70:D8:62:D2:1E', 'CE:36:A9:BE:A2:7C', '2B:9F:E8:54:2A:EA', '0A:3B:35:BA:9D:7E'],
+        'table': 'Bageldom',
+        'site': 'Floor 1',
+        'site_id': 'Bageldom_L2_S1',
+        'zone': 'Smoked Meat Room front',
+        'zone_id': 'Bageldomd_L2_S1_Z1',
+        'mac': '9A:2E:46:13:87:9A'
+    },
+    'Bageldom5634 Z2': {
+        'devices': ['89:E1:94:69:B3:6F', 'B8:2A:37:80:BE:D5', 'F4:F9:20:21:70:20', 'E9:CB:D6:CC:C9:9E'],
+        'table': 'Bageldom',
+        'site': 'Floor 1',
+        'site_id': 'Bageldom_L2_S2',
+        'zone': 'Smoked Meat Room back',
+        'zone_id': 'Bageldomd_L2_S1_Z2',
+        'mac': '73:74:4C:57:CC:9C'
+    },
+    'Phodo7742 Z1': {
+        'devices': ['86:B6:BE:0A:4D:73', '90:7F:65:50:33:92', 'DE:54:8C:67:F9:8E', '3A:5A:01:26:37:44'],
+        'table': 'Phodo',
+        'site': 'Lobby',
+        'site_id': 'Phodo_L1_S1',
+        'zone': 'Siracha Showcase',
+        'zone_id': 'Phodo_L1_S1_Z1',
+        'mac': 'A4:8F:E7:97:E2:5E'
+    },
+    'Phodo7742 Z2': {
+        'devices': ['86:09:F0:3A:5F:44', 'F3:C3:B6:94:C2:FA', '68:93:7C:96:6A:4D', 'EF:78:1F:CB:84:4D'],
+        'table': 'Phodo',
+        'site': 'Lobby',
+        'site_id': 'Phodo_L1_S2',
+        'zone': 'Bean Sprouts Showcase',
+        'zone_id': 'Phodo_L1_S1_Z2',
+        'mac': '85:B9:C9:1C:AB:A9'
+    },
+    'Phodo7742 Z3': {
+        'devices': ['47:41:38:1D:3A:6D', '75:A0:91:EE:6D:0B', '71:FC:43:4D:74:6A', 'DF:CC:0E:C5:AF:8D'],
+        'table': 'Phodo',
+        'site': 'Lobby',
+        'site_id': 'Phodo_L1_S3',
+        'zone': 'Noodle Showcase',
+        'zone_id': 'Phodo_L1_S1_Z3',
+        'mac': 'B7:8B:A4:D6:B8:F6'
+    }
+}
+
---
+++ main.py
@@ -0,0 +1,66 @@
+import json
+import random
+from fake_mapping import mapping
+import numpy as np
+
+NEB_DURATION = 300
+STUDY_DURATION = 3600
+BACKGROUND_DURATION = 300
+
+EPOCH_START = 1641686248
+EPOCH_END = 1670543872
+
+def get_key_times(neb_start):
+    template = {
+            "NebStart": neb_start,
+            "NebEnd": neb_start + NEB_DURATION,
+            "DepStart": neb_start - BACKGROUND_DURATION,
+            "DepEnd": neb_start + STUDY_DURATION
+        }
+    return template
+
+
+
+def make_json():
+    locations = list(mapping.keys())
+
+    site_chosen = locations[random.randint(0, len(locations)-1)]
+    print(site_chosen)
+    site_data = mapping[site_chosen]
+    start_time = random.randint(EPOCH_START, EPOCH_END)
+
+    eACH_dic = dict(zip(mapping[site_chosen]['devices'], [random.uniform(0,10) for x in range(0,len(mapping[site_chosen]['devices']))]))
+    eACH_score = np.array(list(eACH_dic.values())).mean()
+    mock_json = {
+        "id": site_chosen,
+        "sensors": {
+            "time": start_time,
+            "location": site_chosen,
+            "device_eACH": eACH_dic,
+            "zone_eACH": eACH_score,
+            "zone": mapping[site_chosen]["zone"],
+            "site": mapping[site_chosen]["site"],
+            "zone_id": mapping[site_chosen]["zone_id"],
+            "site_id": mapping[site_chosen]["site_id"],
+            "key_times": get_key_times(start_time),
+            "occupancy": int(random.randint(0, 20)),
+            "stay_safe_time": round(random.uniform(10.5, 300), 2),
+            "stay_safe_occupancy": round(random.uniform(2, 50), 2),
+            "vent_per_person":round(random.uniform(10.5, 75.5), 2),
+            "infection_risk": round(random.uniform(10.5, 75.5), 2)
+            },
+        "type": "result"
+    }
+    print(mock_json)
+    return mock_json
+
+make_json()
+
+dumm_list = []
+for time in range(50):
+    dumm_list.append(make_json())
+with open("DummyData.json", 'r+') as dummyData_file:
+    dummyJSON = {
+        'DummyData': dumm_list
+    }
+    json.dump(dumm_list, dummyData_file, indent = 4)
